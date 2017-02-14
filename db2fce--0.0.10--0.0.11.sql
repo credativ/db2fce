ff268452dc1d05e5f1f6e453f6e133b1f0e49f6c
Balázs Bárány <balazs@tud.at>
2017-02-14 17:10:21 +0100

    Removed whitespace from line endings

--- db2fce--0.0.10--0.0.11.sql
+++ db2fce--0.0.10--0.0.11.sql
@@ -1,5 +1,5 @@
 -- TIMESTAMP_FORMAT(): Converts a text to a timestamp using the supplied time format.
--- Most format strings are compatible between DB2 and PostgreSQL. 
+-- Most format strings are compatible between DB2 and PostgreSQL.
 CREATE OR REPLACE FUNCTION db2.timestamp_format(text, text)
 RETURNS TIMESTAMP WITHOUT TIME ZONE
 AS
@@ -9,7 +9,7 @@ $FUNC$
         -- Digits after the 6th are added to the seconds which is not desirable.
         -- So we change the FF format string to US (microseconds) and truncate the digits to 6.
         case when $2 ~ '\.FF\d*$' then
-            to_timestamp(regexp_replace($1, '\.(\d{1,6})\d*$', '.\1'), 
+            to_timestamp(regexp_replace($1, '\.(\d{1,6})\d*$', '.\1'),
                          regexp_replace($2, '\.FF\d*$', '.US'))
         -- Try the original format. Most format strings seem to be identical between PostgreSQL and DB2.
         else
